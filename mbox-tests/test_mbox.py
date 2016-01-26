198d9b1f0a0a618a34246f40d8e2ae5f7b002fae
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2016-01-26 15:50:25 +0000

    test_mbox: modify test case to match latest patchtest code
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- mbox-tests/test_mbox.py
+++ mbox-tests/test_mbox.py
@@ -1,46 +1,50 @@
 from subprocess import Popen, PIPE
 import os
-import patchtest
+from patchtestargs import PatchTestArgs as pta
+from repo import Repo
 import unittest
 
+def find_lines(path, key):
+    """ Returns the lines containing the key """
+    _lines = []
+    with open(path) as f:
+        for line in f.readlines():
+            if line.find(key) != -1:
+                _lines.append(line)
+    return _lines
+
+@unittest.skipUnless(pta.mbox, "requires the mbox argument")
 class TestMbox(unittest.TestCase):
     """ A testcase containing (mbox formatted) patch related tests"""
+
+    valid_upstream_status = ['Pending', 'Submitted', 'Accepted', 'Backport', 'Denied', 'Inappropriate']
+
     @classmethod
     def setUpClass(cls):
         # consider these variables monkey typed
-        cls.mbox = patchtest.PatchTestArgs.mbox
-
-	def find_key(path, key):
-		""" From oelint.bbclass, finds key in path (file)"""
-		ret = True
-		f = file('%s' % path, mode = 'r')
-		line = f.readline()
-		while line:
-			if line.find(key) != -1:
-				ret = False
-			line = f.readline()
-		f.close()
-		return ret
+        cls.mbox = os.path.abspath(pta.mbox)
 
     def test_patch_files(self):
-        """ Check if the mbox items are actual files"""
-        for file in cls.mbox:
-            self.assertTrue(os.path.isfile(file))
+        """ Check if the mbox items are actual files """
+        self.assertTrue(os.path.isfile(TestMbox.mbox))
 
     def test_signed_off_by(self):
-        """ Check signed off by is present in each file"""
-        for file in cls.mbox:
-            self.assertTrue(find_key(os.path.abspath(file), 'Signed-off-by: '))
+        """ Check signed off by is present in each file """
+        self.assertTrue(find_lines(TestMbox.mbox, 'Signed-off-by:'))
 
     def test_upsteam_status(self):
-        """ Check upstream status is present in each file"""
-        for file in cls.mbox:
-            self.assertTrue(find_key(os.path.abspath(file), 'Upstream-Status: '))
+        """ Check upstream status is present in each file """
+        for line in find_lines(TestMbox.mbox, 'Upstream-Status:'):
+            self.assertTrue([valid for valid in TestMbox.valid_upstream_status if valid in line], "Invalid Upstream-Status: %s" % line)
 
-    def test_summary_length(self):
-        """ Check for a minimum summary length"""
-        #for file in cls.mbox:
-		#	summary = None #what do I do here
-		#	check if patchwork has this
-		self.assertTrue(True)
+    # def test_summary_length(self):
+    #     """ Check for a summary length"""
+    #     lines = find_lines(TestMbox.mbox, 'Subject:')
+    #     self.assertTrue(line, 'summary should be present')
+    #     subject = lines[0]
+    #     liens.split(':')
+    #     #for file in cls.mbox:
+    #     	#	summary = None #what do I do here
+    #     	#	check if patchwork has this
+    #     	self.assertTrue(True)
 

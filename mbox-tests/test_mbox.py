394bdc5e3601129e494923e5453a63a5c534012e
Alex Franco <alejandro.franco@linux.intel.com>
2016-03-08 17:18:15 -0600

    mbox-tests: removed duplicate tests
    Signed-off-by: Alex Franco <alejandro.franco@linux.intel.com>

--- mbox-tests/test_mbox.py
+++ mbox-tests/test_mbox.py
@@ -2,8 +2,11 @@ import os
 import re
 import unittest
 import requests
-from patchtestdata import PatchTestInput as pti
 import sys
+from patchtestdata import PatchTestInput as pti
+from difflib import context_diff as diffs
+from patchtestdata import PatchTestDataStore as ds
+import logging
 
 @unittest.skipUnless(pti.mbox or pti.series, "requires the mbox or series argument")
 class TestMbox(unittest.TestCase):
@@ -11,85 +14,77 @@ class TestMbox(unittest.TestCase):
 
     valid_upstream_status = ['Pending', 'Submitted', 'Accepted', 'Backport', 'Denied', 'Inappropriate']
     max_len = 78
+    logger = logging.getLogger('patchtest')
 
     @classmethod
     def setUpClass(cls):
         cls.items = pti.repo.items
 
-        for _item in cls.items:
-            if _item.is_empty:
-                raise(AssertionError, 'mbox should not be empty')
-
-    def test_signed_off_by(self):
-        """ Check Signed-off-by presence"""
+    def test_patch_format(self):
+        """ Format check: patch is not empty and changes files"""
+        # Checks for emptiness and changed files
         for _item in self.items:
-            _keyvals = _item.keyvals
-            _signoff = True if 'Signed-off-by' in _keyvals else False
-            self.assertTrue(_signoff, 'Signed-off-by absent from commit message')
-            if _signoff:
-                self.assertTrue(''.join(_keyvals['Signed-off-by']).strip(), 'Singed-off-by should not be empty')
+            self.assertFalse(_item.is_empty, 'Mbox should not be empty')
+            self.assertGreater(len(_item.changes), 0, "There should be changed files")
 
-    def test_signed_off_by_spelling(self):
-        """ Check Signed-off-by correct spelling"""
+    def test_signed_off_by_check(self):
+        """ Check Signed-off-by is present and spelled right"""
         for _item in self.items:
             _keys = _item.keyvals.keys()
             _lc_keys = set( ( k.lower() for k in _keys ) )
             _alt = set( ('signed-off-by', 'signed-off_by', 'signed_off-by', 'signed_off_by') )
-            if not _alt.intersection(_lc_keys):
-                raise unittest.SkipTest('Signed-off-by must be present to check its spelling')
-            else:
-                self.assertTrue('Signed-off-by' in _keys, "Signed-off-by seems to be mispelled")
 
-    def test_upstream_status_spelling(self):
-        """ Check Upstream-Status correct spelling"""
+            if not _alt.intersection(_lc_keys):
+                raise AssertionError('Signed-off-by must be present to check its spelling')
+            else:
+                self.assertIn('Signed-off-by', _keys, "Signed-off-by seems to be mispelled")
+
+    def test_upstream_status_check(self):
+        """ Check Upstream-Status spelling and value if present"""
         for _item in self.items:
             _keys = _item.keyvals.keys()
             _lc_keys = [k.lower() for k in _keys]
+
             if not ('upstream-status' in _lc_keys or 'upstream_status' in _lc_keys):
-                raise unittest.SkipTest('Upstream-Status must be present to check its spelling')
+                raise unittest.SkipTest('Upstream-Status must be present to validate it')
             else:
                 self.assertTrue('Upstream-Status' in _keys, "Upstream-Status seems to be mispelled")
-
-    def test_upstream_status_value(self):
-        """ Check Upstream-Status is if present"""
-        for _item in self.items:
-            if 'Upstream-Status' not in _item.keyvals:
-                raise unittest.SkipTest('Upstream-Status must be present to check its validity')
-            else:
                 for _st in _item.keyvals['Upstream-Status']:
                     self.assertTrue([valid for valid in TestMbox.valid_upstream_status if valid == _st], "Invalid Upstream-Status: %s" % _st)
 
-    def test_subject(self):
-        """ Check Subject presence"""
+    def test_subject_check(self):
+        """ Check Subject presence, length, contents"""
+        _key = 'Subject'
         for _item in self.items:
-            self.assertTrue('Subject' in _item.keyvals, "Subject should be in mbox commit message")
-
-    def test_subject_length(self):
-        """ Check Subject length if present"""
-        for _item in self.items:
-            _key = 'Subject'
             if _key not in _item.keyvals:
-                raise unittest.SkipTest('Subject must be present to check its length')
+                raise AssertionError('%s line missing' % _key)
             else:
-                _val = _item.keyvals[_key]
-                self.assertTrue(len(_val) <= TestMbox.max_len, "%s too long, should be at most %s characters. Its value is '%s'" % (_key, TestMbox.max_len, _val))
+                _val = ''.join(_item.keyvals[_key]).strip()
+                self.assertLessEqual(len(_val), TestMbox.max_len, "%s too long, should be at most %s characters. Its value is '%s'" % (_key, TestMbox.max_len, _val))
+                self.assertIn(':', _val, "%s doesn't include a colon, possibly missing component")
 
-    def test_description(self):
-        """ Check Description presence"""
+    def test_long_log(self):
+        """ Check long log presence, contents"""
+        _key = 'Description'
         for _item in self.items:
-            _hasdesc = True if 'Description' in _item.keyvals else False
-            self.assertTrue(_hasdesc, "A Description should exist")
+            _hasdesc = True if _key in _item.keyvals.keys() else False
+            self.assertTrue(_hasdesc, "A long log should exist")
             if _hasdesc:
-                self.assertTrue(''.join(_item.keyvals['Description']).strip(), 'Description should not be empty')
+                self.assertTrue(''.join(_item.keyvals[_key]).strip(), 'Long log should not be empty')
+                self.assertNotEquals(''.join(_item.keyvals[_key]).strip(), ''.join(_item.keyvals['Subject']).strip(), "Short and long logs should not be the same")
 
-    def test_changes_exist(self):
-        """ Check there are changed files"""
-        for _item in self.items:
-            changes = _item.changes
-            self.assertTrue(len(changes) > 0, "There should be changed files")
+    def pretest_pylint(self):
+        """ Run pylint on all modified python files, pre-merge"""
+        self.common_pylint()
 
     def test_pylint(self):
-        """ Obtain changed python lines, compare with pylint"""
+        """ Run pylint on all modified python files, post-merge"""
+        self.common_pylint()
+
+    def common_pylint(self):
+        """ Run pylint on all modified python files"""
+
+        # Find changed python files
         pych = {}
         for _item in self.items:
             if _item.changes:
@@ -97,11 +92,27 @@ class TestMbox(unittest.TestCase):
                     pych[pf.path] = []
         if not pych:
             raise unittest.SkipTest('Python changes must exist to run pylint')
+
+        # Run pylint on changed files and populate pych with findings
+        from pylint import epylint as lint
+        for pf in pych.keys():
+            (pylo, _) = lint.py_run(pf, return_std=True)
+            pych[pf] += [ line.strip() for line in pylo.readlines()[1:] ]
+
+        # Store the changes in data store
+        ds['pylint_new'] = set()
+        for chglist in pych.values():
+            for chg in chglist:
+                if chg:
+                    ds['pylint_new'].add(chg)
+
+        # Handle lint differences
+        pylint_diff = set()
+        if ds['pylint_old']:
+            # Running post-merge test
+            pylint_diff = ds['pylint_old'] ^ ds['pylint_new']
+            self.assertEquals(len(pylint_diff), 0, "Found new pylint issues: %s" % str(pylint_diff))
         else:
-            from pylint import epylint as lint
-            for pf in pych.keys():
-                (pylo, _) = lint.py_run(pf, return_std=True)
-                pych[pf] += [ line.strip() for line in pylo.readlines()[1:] ]
-                #for line in pych[pf]:
-                #    print("%s" % line)
-                self.assertFalse([pf for pf in pych.keys() if len(pych[pf]) > 0 ],"Any pylint output causes a failure")
+            # Running pre-merge test
+            ds['pylint_old'] = ds['pylint_new']
+            self.assertEquals(len(ds['pylint_new']), 0, "There are pylint issues")
